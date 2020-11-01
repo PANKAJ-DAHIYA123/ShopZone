65b061e2e8b3f1bda3199be08123e9ce7c42e9b8
Pankaj Dahiya <pankaj.id.008@gmail.com>
2020-11-01 20:34:43 +0530

    import os for os.getenv()

--- news.js
+++ news.js
@@ -1,1 +1,2 @@
-document.getElementById('sample').innerHTML=`Printing ${getenv('NEWS_API_KEY')}`;
+import os from 'os';
+document.getElementById('sample').innerHTML=`Printing ${os.getenv('NEWS_API_KEY')}`;
