a1db12bd28f2f5822cdcad3c26b5ee0ace3ac9fb
Pankaj Dahiya <pankaj.id.008@gmail.com>
2020-11-02 20:04:19 +0530

    process to PROCESS

--- public/news.js
+++ public/news.js
@@ -1,2 +1,3 @@
-console.log(process.env.NEWS_API_KEY);
-document.getElementById("news").innerHTML=process.env.NEWS_API_KEY;
+console.log(PROCESS.ENV.NEWS_API_KEY);
+console.log(PROCESS.env.NEWS_API_KEY);
+document.getElementById("news").innerHTML=PROCESS.env.NEWS_API_KEY;
