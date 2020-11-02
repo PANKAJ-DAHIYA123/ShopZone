7a91946057fab60e787b15d7afb63c03fa90599b
Pankaj Dahiya <pankaj.id.008@gmail.com>
2020-11-02 13:38:37 +0530

    Prettier formatting

--- public/news.js
+++ public/news.js
@@ -1,2 +1,4 @@
-import os from 'os';
-document.getElementById('sample').innerHTML=`Printing ${os.getenv('NEWS_API_KEY')}`;
+import os from "os";
+document.getElementById("sample").innerHTML = `Printing ${os.getenv(
+  "NEWS_API_KEY"
+)}`;
