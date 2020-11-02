d09fd325ce8575a15ca00f020f089a7c42055531
Pankaj Dahiya <pankaj.id.008@gmail.com>
2020-11-02 11:16:23 +0530

    converting php to node

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log("newszone app is running on port: " + port);
+});
