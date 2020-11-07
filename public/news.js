466b666c5c0eae7d5f2c98e1683e6fd8cb7f5857
Pankaj Dahiya <pankaj.id.008@gmail.com>
2020-11-07 21:03:18 +0530

    used axios to get news responses

--- public/news.js
+++
@@ -1,6 +0,0 @@
-const createRequire = require("module");
-const require = createRequire(import.meta.url);
-
-const key = require("../server.js");
-console.log(key);
-document.getElementById("news").innerHTML = "abcd";
--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 var express = require("express");
+var axios = require("axios");
+const { response } = require("express");
 var app = express();
 require("dotenv").config();
 var port = process.env.PORT || 5500;
-module.exports = process.env.NEWS_API_KEY;
+
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
 
-app.get("/", (req, res) => {
-  res.render("index");
+function getNews() {
+  var url =
+    "http://newsapi.org/v2/top-headlines?" +
+    "country=us&" +
+    "apiKey=46b1aea1d63e44e1a1eaf325874f39c5";
+  axios({ url })
+    .then((response) => {
+      return response.data.articles;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+app.get("/", async (req, res) => {
+  const news =await getNews();
+  res.render("index", { news: news });
 });
 
 app.listen(port, () => {
