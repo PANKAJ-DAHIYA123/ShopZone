5a8a45ef026466672df4c847a45f246303c28033
Pankaj Dahiya <pankaj.id.008@gmail.com>
2020-11-10 11:40:29 +0530

    made headlines and categories menu work

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
-  var url =
-    "http://newsapi.org/v2/top-headlines?" + "country=us&" + `apiKey=${key}`;
+  var url = "http://newsapi.org/v2/top-headlines?" + "country=in&" + `apiKey=${key}`;
   axios
     .get(url)
     .then((response) => {
@@ -25,7 +24,76 @@ app.get("/", (req, res) => {
       console.log(error);
     });
 });
-
+app.get("/Business", (req, res) => {
+  var url =
+    "http://newsapi.org/v2/everything?" + "q=Business&" + `apiKey=${key}`;
+  axios
+    .get(url)
+    .then((response) => {
+      res.render("index", { news: response.data.articles });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+app.get("/Technology", (req, res) => {
+  var url =
+    "http://newsapi.org/v2/everything?" + "q=Technology&" + `apiKey=${key}`;
+  axios
+    .get(url)
+    .then((response) => {
+      res.render("index", { news: response.data.articles });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+app.get("/Entertainment", (req, res) => {
+  var url =
+    "http://newsapi.org/v2/everything?" + "q=Entertainment&" + `apiKey=${key}`;
+  axios
+    .get(url)
+    .then((response) => {
+      res.render("index", { news: response.data.articles });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+app.get("/Sports", (req, res) => {
+  var url = "http://newsapi.org/v2/everything?" + "q=Sports&" + `apiKey=${key}`;
+  axios
+    .get(url)
+    .then((response) => {
+      res.render("index", { news: response.data.articles });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+app.get("/Science", (req, res) => {
+  var url =
+    "http://newsapi.org/v2/everything?" + "q=Science&" + `apiKey=${key}`;
+  axios
+    .get(url)
+    .then((response) => {
+      res.render("index", { news: response.data.articles });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+app.get("/Health", (req, res) => {
+  var url = "http://newsapi.org/v2/everything?" + "q=Health&" + `apiKey=${key}`;
+  axios
+    .get(url)
+    .then((response) => {
+      res.render("index", { news: response.data.articles });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 app.listen(port, () => {
   console.log("newszone app is running on port: " + port);
 });
