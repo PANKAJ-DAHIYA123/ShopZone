0fc6e778afbdb36dec81ae34d02b8f87022688a1
Pankaj Dahiya <pankaj.id.008@gmail.com>
2020-11-10 10:54:16 +0530

    displayed headlines but still big

--- server.js
+++ server.js
@@ -4,26 +4,26 @@ const { response } = require("express");
 var app = express();
 require("dotenv").config();
 var port = process.env.PORT || 5500;
-function getNews(key) {
-  var url =
-    "http://newsapi.org/v2/top-headlines?" + "country=us&" + `apiKey=${key}`;
-  axios({ url })
-    .then((response) => {
-      return response.data.articles;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+var key = process.env.NEWS_API_KEY;
+async function getNews(key) {
+  return news;
 }
+
 app.set("view engine", "ejs");
 
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
-  res.render("index", {
-    key: process.env.NEWS_API_KEY,
-    getNews: getNews,
-  });
+  var url =
+    "http://newsapi.org/v2/top-headlines?" + "country=us&" + `apiKey=${key}`;
+  axios
+    .get(url)
+    .then((response) => {
+      res.render("index", { news: response.data.articles });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 app.listen(port, () => {
