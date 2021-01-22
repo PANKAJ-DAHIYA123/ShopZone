030718032d1d2a744517b3372e2b4ffef4cc9eb1
Pankaj Dahiya <pankaj.id.008@gmail.com>
2021-01-22 21:02:03 +0530

    header and footer improvements , fixing searchbar

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ var app = express();
 require("dotenv").config();
 var port = process.env.PORT || 5500;
 var key = process.env.NEWS_API_KEY;
-async function getNews(key) {
-  return news;
-}
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
-  var url = "http://newsapi.org/v2/top-headlines?" + "country=in&" + `apiKey=${key}`;
+  var url =
+    "http://newsapi.org/v2/top-headlines?" + "country=in&" + `apiKey=${key}`;
   axios
     .get(url)
     .then((response) => {
@@ -85,6 +86,20 @@ app.get("/Science", (req, res) => {
 });
 app.get("/Health", (req, res) => {
   var url = "http://newsapi.org/v2/everything?" + "q=Health&" + `apiKey=${key}`;
+  axios
+    .get(url)
+    .then((response) => {
+      res.render("index", { news: response.data.articles });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+app.post("/Search", (req, res) => {
+  var url =
+    "http://newsapi.org/v2/everything?" +
+    `q=${req.body.query}&` +
+    `apiKey=${key}`;
   axios
     .get(url)
     .then((response) => {
