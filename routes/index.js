d226072a7ad7b91533ffb8ed42615407ce88a831
Pankaj Dahiya <pankaj.id.008@gmail.com>
2021-01-27 23:01:16 +0530

    fallback star endpoint moved to bottom

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 require("dotenv").config();
 var key = process.env.NEWS_API_KEY;
 
-router.get("*", (req, res) => {
+router.get("/", (req, res) => {
   var url =
     "http://newsapi.org/v2/top-headlines?" + "country=in&" + `apiKey=${key}`;
   axios
@@ -116,6 +116,18 @@ router.get("Science", (req, res) => {
 
 router.get("Health", (req, res) => {
   var url = "http://newsapi.org/v2/everything?" + `q=health&` + `apiKey=${key}`;
+  axios
+    .get(url)
+    .then((response) => {
+      res.render("home", { news: response.data.articles });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+router.get("*", (req, res) => {
+  var url =
+    "http://newsapi.org/v2/top-headlines?" + "country=in&" + `apiKey=${key}`;
   axios
     .get(url)
     .then((response) => {
